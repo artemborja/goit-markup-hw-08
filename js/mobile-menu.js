d8e6ef0ef0930e4ea9cf20e0e431a22dc67f71b8
artemborja <lazerf@i.ua>
2022-07-15 19:52:04 +0300

    modal

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+/*(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("clic", () => {
+    const expanded =
+      manuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();*/
